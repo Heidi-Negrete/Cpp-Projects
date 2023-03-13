bc08042e1ce9b203f631a202d290027fa2225e46
Heidi Negrete <heidi@negrete.dev>
2023-03-13 11:48:01 -0400

    add function to request number from user and return a double

--- BasicCalculator.cpp
+++ BasicCalculator.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include <string>
+
+double getDouble() {
+	std::cout << "Enter a number. ";
+	double userNumber{};
+	std::cin >> userNumber;
+	return userNumber;
+}
 
 int main()
 {
