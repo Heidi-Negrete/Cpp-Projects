4a164bef76d7892fb812d982c979ce56a7da8da2
Heidi Negrete <heidi@negrete.dev>
2023-03-15 11:01:02 -0400

    Program tests user iput integer and prints whether odd or even

--- EvenOrOdd/EvenOrOdd.cpp
+++ EvenOrOdd/EvenOrOdd.cpp
@@ -10,4 +10,13 @@ bool isEven(int x) {
 int main()
 {
     std::cout << "Enter an integer. ";
+    int x{};
+    std::cin >> x;
+    
+    if (isEven(x))
+        std::cout << x << " is even\n";
+    else
+        std::cout << x << " is odd\n";
+
+    return 0;
 }
